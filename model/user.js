eb38911fe611c9103fd1d5b4a8a1c2309824e534
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2022-01-15 00:34:08 +0200

    file-storage

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 import pkg from 'mongoose'
 import bcrypt from 'bcryptjs'
+import gravatar from 'gravatar'
 import { Role } from '../lib/constants'
 
 const { Schema, model } = pkg
@@ -34,6 +35,12 @@ const userSchema = new Schema(
     token: {
       type: String,
       default: null,
+    },
+    avatar: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: '250'}, true )
+      },
     },
   },
   {
