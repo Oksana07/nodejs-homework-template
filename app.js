2e6a38af6928f4bed5cd2bd0bd3fed514cf4406b
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2022-01-14 11:36:41 +0200

    fix

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json({ limit: LIMIT_JSON })) // json
+app.use(express.json({ limit: LIMIT_JSON }))
 app.use((req, res, next) => {
-  app.set('lang', req.acceptsLanguages(['en', 'ru']))
+  app.set('lang', req.acceptsLanguages(['en', 'ru', 'ua']))
   next()
 })
 
